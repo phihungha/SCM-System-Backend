e1eebb23f15ad11470a19931b42594c3965ce5f7
Ha Phi Hung <haphihung55@gmail.com>
2023-10-20 18:43:42 +0700

    Fix non-action method considered as action

--- ScmssApiServer/Utilities/CustomControllerBase.cs
+++ ScmssApiServer/Utilities/CustomControllerBase.cs
@@ -4,6 +4,7 @@ namespace ScmssApiServer.Utilities
 {
     public abstract class CustomControllerBase : ControllerBase
     {
+        [NonAction]
         public OkObjectResult OkMessage(string message)
         {
             return Ok(new { Title = message });
