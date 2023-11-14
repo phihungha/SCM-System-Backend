b2202c579a1cf7b9cb507fe119e317475b875111
Ha Phi Hung <haphihung55@gmail.com>
2023-11-14 19:02:07 +0700

    Interface for sales order domain service

---
+++ ScmssApiServer/IDomainServices/ISalesOrderService.cs
@@ -0,0 +1,24 @@
+﻿using ScmssApiServer.DTOs;
+using ScmssApiServer.Models;
+
+namespace ScmssApiServer.IDomainServices
+{
+    public interface ISalesOrderService
+    {
+        Task<IList<SalesOrder>> GetSalesOrdersAsync();
+
+        Task<SalesOrder?> GetSalesOrderAsync(int id);
+
+        Task<SalesOrder> CreateSalesOrderAsync(SalesOrderInputDto dto, string userId);
+
+        Task<SalesOrder> UpdateSalesOrderAsync(int id, SalesOrderInputDto dto);
+
+        Task<SalesOrder> CancelSalesOrderAsync(int id, string userId);
+
+        Task<SalesOrder> CompleteSalesOrderAsync(int id, string userId);
+
+        Task<SalesOrderProgressUpdate> CreateProgressUpdateAsync(int id, OrderProgressUpdateInputDto dto);
+
+        Task<SalesOrderProgressUpdate> EditProgressUpdateAsync(int id, OrderProgressUpdateInputDto dto);
+    }
+}
