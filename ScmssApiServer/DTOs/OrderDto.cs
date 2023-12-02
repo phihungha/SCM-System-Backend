06c18f5a301d5362ca602ad1bd3762329841dc20
Ha Phi Hung <haphihung55@gmail.com>
2023-12-02 18:53:05 +0700

    Add DTO interface for lifecycle models

--- ScmssApiServer/DTOs/OrderDto.cs
+++ ScmssApiServer/DTOs/OrderDto.cs
@@ -2,30 +2,20 @@
 
 namespace ScmssApiServer.DTOs
 {
-    public abstract class OrderDto<TItem, TEvent>
+    public abstract class OrderDto<TItem, TEvent> : ILifecycle
         where TItem : OrderItemDto
         where TEvent : OrderEventDto
     {
         public DateTime CreateTime { get; set; }
-
         public UserDto CreateUser { get; set; } = null!;
-
         public required string CreateUserId { get; set; }
-
+        public DateTime? EndTime { get; set; }
+        public UserDto? EndUser { get; set; }
+        public string? EndUserId { get; set; }
         public ICollection<TEvent> Events { get; set; } = new List<TEvent>();
-
         public DateTime? ExecutionFinishTime { get; set; }
-
-        public DateTime? FinishTime { get; set; }
-
-        public UserDto? FinishUser { get; set; }
-
-        public string? FinishUserId { get; set; }
-
         public int Id { get; set; }
-
         public ICollection<TItem> Items { get; set; } = new List<TItem>();
-
         public string? Problem { get; set; }
         public OrderStatus Status { get; set; }
         public DateTime? UpdateTime { get; set; }
