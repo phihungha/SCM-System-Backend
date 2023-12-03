e5e148ddcb0ce1647e70120b01b51e7bd0e6c670
Ha Phi Hung <haphihung55@gmail.com>
2023-12-04 01:22:09 +0700

    Setup basic purchase order service

--- ScmssApiServer/DTOs/PurchaseOrderUpdateDto.cs
+++ ScmssApiServer/DTOs/PurchaseOrderUpdateDto.cs
@@ -2,7 +2,7 @@
 {
     public class PurchaseOrderUpdateDto : TransOrderUpdateDto<PurchaseOrderItemUpdateDto>
     {
-        public decimal DiscountAmount { get; set; }
+        public decimal? DiscountAmount { get; set; }
         public string? FromLocation { get; set; }
         public Uri? InvoiceUrl { get; set; }
         public Uri? ReceiptUrl { get; set; }
---
+++ ScmssApiServer/DomainServices/PurchaseOrdersService.cs
@@ -0,0 +1,38 @@
+﻿using ScmssApiServer.DTOs;
+using ScmssApiServer.IDomainServices;
+
+namespace ScmssApiServer.DomainServices
+{
+    public class PurchaseOrdersService : IPurchaseOrdersService
+    {
+        public Task<TransOrderEventDto> AddManualEventAsync(int orderId, TransOrderEventCreateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PurchaseOrderDto> CreateAsync(PurchaseOrderCreateDto dto, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PurchaseOrderDto?> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<PurchaseOrderDto>> GetManyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PurchaseOrderDto> UpdateAsync(int id, PurchaseOrderUpdateDto dto, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TransOrderEventDto> UpdateEventAsync(int id, int orderId, OrderEventUpdateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
