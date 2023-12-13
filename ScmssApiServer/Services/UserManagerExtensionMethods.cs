583861e0717bbbfae69473016ded783d6f74990f
Ha Phi Hung <haphihung55@gmail.com>
2023-12-13 18:23:03 +0700

    Remove unneeded role methods

--- ScmssApiServer/Services/UserManagerExtensionMethods.cs
+++ ScmssApiServer/Services/UserManagerExtensionMethods.cs
@@ -6,44 +6,16 @@ namespace ScmssApiServer.Services
 {
     public static class UserManagerExtensionMethods
     {
-        public static async Task<User> FindFullUserByIdAsync(
-            this UserManager<User> manager,
-            string id)
+        /// <summary>
+        /// Find by ID and return a User with populated Roles.
+        /// </summary>
+        /// <param name="id">User ID</param>
+        /// <returns></returns>
+        public static async Task<User> FindFullUserByIdAsync(this UserManager<User> manager, string id)
         {
-            User user = await manager.Users.Include(i => i.ProductionFacility)
-                                           .FirstAsync(i => i.Id == id);
+            User user = await manager.Users.FirstAsync(i => i.Id == id);
             user.Roles = await manager.GetRolesAsync(user);
             return user;
-        }
-
-        /// <summary>
-        /// Get a list of role names the specified user belongs to.
-        /// </summary>
-        /// <param name="userId">User ID</param>
-        /// <returns>Role names</returns>
-        /// <exception cref="KeyNotFoundException"></exception>
-        public static async Task<IList<string>> GetRolesAsync(
-            this UserManager<User> manager,
-            string userId)
-        {
-            User user = await manager.FindFullUserByIdAsync(userId);
-            return user.Roles;
-        }
-
-        /// <summary>
-        /// Return True if a user is in any of the specified roles.
-        /// </summary>
-        /// <param name="userId">User ID</param>
-        /// <param name="rolesToCheck">Roles to check</param>
-        /// <returns>True if user has a role in the specified roles.
-        /// False otherwise.</returns>
-        public static async Task<bool> IsInAnyRolesAsync(
-            this UserManager<User> manager,
-            string userId,
-            params string[] rolesToCheck)
-        {
-            IList<string> roles = await manager.GetRolesAsync(userId);
-            return roles.Any(i => rolesToCheck.Contains(i));
         }
     }
 }
