8429c49c371acb5b0ba5b79717795514264258d5
Ha Phi Hung <haphihung55@gmail.com>
2023-12-26 22:49:51 +0700

    Fix production order create type

--- ScmssApiServer/DTOs/OrderCreateDto.cs
+++ ScmssApiServer/DTOs/OrderCreateDto.cs
@@ -2,7 +2,7 @@
 
 namespace ScmssApiServer.DTOs
 {
-    public abstract class OrderCreateDto<TItemDto>
+    public class OrderCreateDto<TItemDto>
     {
         [Required]
         public required ICollection<TItemDto> Items { get; set; }
