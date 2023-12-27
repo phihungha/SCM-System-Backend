a28166be8edfb8d6748d3ebca5e7d6cb36c11c54
Ha Phi Hung <haphihung55@gmail.com>
2023-12-27 23:01:50 +0700

    Validate report query

--- ScmssApiServer/DTOs/ReportQueryDto.cs
+++ ScmssApiServer/DTOs/ReportQueryDto.cs
@@ -1,10 +1,19 @@
-﻿namespace ScmssApiServer.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScmssApiServer.DTOs
 {
     public class ReportQueryDto
     {
+        [Range(1, 12)]
         public int EndMonth { get; set; }
+
+        [Range(1970, int.MaxValue)]
         public int EndYear { get; set; }
+
+        [Range(1, 12)]
         public int StartMonth { get; set; }
+
+        [Range(1970, int.MaxValue)]
         public int StartYear { get; set; }
     }
 }
